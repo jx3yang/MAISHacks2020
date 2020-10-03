96a28eb651335673ad17ed4315c005393c85fb1f
Ji Xi Yang <jx3yang@uwaterloo.ca>
2020-10-03 19:07:28 -0400

    Sentiment analysis API

---
+++ backend/sentiment_analysis_api/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+import tensorflow as tf
+import numpy as np
+import json
+
+app = Flask(__name__)
+model = tf.keras.models.load_model('model')
+
+index2Sentiment = {
+    0: 'negative',
+    1: 'neutral',
+    2: 'positive'
+}
+
+@app.route('/')
+def home():
+    return json.dumps({'result': 'ok'})
+
+"""
+Schema
+{
+    messages: string[]
+}
+"""
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    if 'messages' not in data:
+        return { 'success': False, 'predictions': [] }
+    predictions = model.predict(data.get('messages'))
+    return {
+        'success': True,
+        'predictions': [index2Sentiment[np.argmax(prediction)] for prediction in predictions]
+    }
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5002)
