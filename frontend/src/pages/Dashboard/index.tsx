16d7827503f53b3de2251f29d4287d2e4455c63d
Ji Xi Yang <jx3yang@uwaterloo.ca>
2020-10-04 09:18:26 -0400

    Hooking up frontend to azure api

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -2,21 +2,76 @@ import React, { useState, useEffect } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Card, Row, Col, } from 'antd';
 import TimelineChart, { TimelineItem } from '@/components/TimelineChart';
-import { fakeTimeSeries } from '@/services/time_series';
+import { fakeTimeSeries, getMetric, runAnomalyDetection } from '@/services/time_series';
 
 const Dashboard: React.FC<{}> = () => {
   const [data, setData] = useState<TimelineItem[]>([]);
+  const [act_1, setAct_1] = useState<TimelineItem[]>([]);
+  const [act_0, setAct_0] = useState<TimelineItem[]>([]);
+  const [conversation, setConversation] = useState<TimelineItem[]>([]);
+  const [phone, setPhone] = useState<TimelineItem[]>([]);
 
-  const makeItem = (x: number, y: number): TimelineItem => {
-    const item: TimelineItem = { x, y, anomaly: y < 1170 };
+  const makeItem = (x: number, y: number, anomaly: boolean): TimelineItem => {
+    const item: TimelineItem = { x, y, anomaly };
     return item;
   }
+
+  const formatSeries = (series) => series.map(point => ({timestamp: point.timestamp.replace(' ', 'T').concat('Z'), value: point.value}));
+  const formatTimestamp = (ts: string) => Date.parse(ts) / 1000;
 
   useEffect(() => {
     fakeTimeSeries().then(res => {
       const series = res.series as ({value: number, timestamp: string}[]);
       const seriesData: TimelineItem[] = series.map(point => makeItem(Date.parse(point.timestamp) / 1000, point.value / 100000));
       setData(seriesData.slice(seriesData.length - 11, seriesData.length - 1));
+    });
+
+    getMetric('act_inf_1').then(res => {
+      const series: any[] = formatSeries(res['series']);
+      runAnomalyDetection(series)
+        .then(res => res.json())
+        .then(res => {
+          const anomalies = res.result.isAnomaly;
+          setAct_1(
+            series.map((elem, index) => makeItem(formatTimestamp(elem.timestamp), elem.value, anomalies[index]))
+          );
+        });
+    });
+
+    getMetric('act_inf_0').then(res => {
+      const series: any[] = formatSeries(res['series']);
+      runAnomalyDetection(series)
+        .then(res => res.json())
+        .then(res => {
+          const anomalies = res.result.isAnomaly;
+          setAct_0(
+            series.map((elem, index) => makeItem(formatTimestamp(elem.timestamp), elem.value, anomalies[index]))
+          );
+        });
+    });
+
+    getMetric('conversation').then(res => {
+      const series: any[] = formatSeries(res['series']);
+      runAnomalyDetection(series)
+        .then(res => res.json())
+        .then(res => {
+          const anomalies = res.result.isAnomaly;
+          setConversation(
+            series.map((elem, index) => makeItem(formatTimestamp(elem.timestamp), elem.value, anomalies[index]))
+          );
+        });
+    });
+
+    getMetric('phone_charging').then(res => {
+      const series: any[] = formatSeries(res['series']);
+      runAnomalyDetection(series)
+        .then(res => res.json())
+        .then(res => {
+          const anomalies = res.result.isAnomaly;
+          setPhone(
+            series.map((elem, index) => makeItem(formatTimestamp(elem.timestamp), elem.value, anomalies[index]))
+          );
+        });
     });
   }, []);
 
@@ -28,32 +83,32 @@ const Dashboard: React.FC<{}> = () => {
       <Row gutter={24}>
         <Col {...colProps}>
           <TimelineChart
-            title='Chart 1'
+            title='Activity Inference Stationary'
             color='#45A1FF'
-            data={data}
+            data={act_0}
           />
         </Col>
         <Col {...colProps}>
           <TimelineChart
-            title='Chart 2'
+            title='Activity Inference Walking'
             color='#9761E5'
-            data={data}
+            data={act_1}
           />
         </Col>
       </Row>
       <Row gutter={24}>
         <Col {...colProps}>
           <TimelineChart
-            title='Chart 3'
+            title='Conversation'
             color='#FBD44C'
-            data={data}
+            data={conversation}
           />
         </Col>
         <Col {...colProps}>
           <TimelineChart
-            title='Chart 4'
+            title='Phone Charging'
             color='#5ACC74'
-            data={data}
+            data={phone}
           />
         </Col>
       </Row>
