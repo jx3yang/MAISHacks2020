d8b8c82897d3416b74e460f9e7270d50469e7f3c
Ji Xi Yang <jx3yang@uwaterloo.ca>
2020-10-09 21:14:24 -0400

    Containerization of the socket service

--- backend/notification_socket/app.py
+++ backend/notification_socket/app.py
@@ -12,4 +12,4 @@ def connect():
     emit('notification', {'message': 'Notification from the backend!'})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='localhost')
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
